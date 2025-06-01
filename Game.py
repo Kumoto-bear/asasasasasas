c14c08cf053e4389ba4ce5b9c90d00e5609dcc76
Cream_Puff <winniebearjayden2.0@gmail.com>
2025-06-01 18:16:02 -0500

    Hello

---
+++ Game.py
@@ -0,0 +1,8 @@
+answer = input("Please enter Yes or No: ").strip().lower()
+
+if answer == "yes":
+    print("You answered Yes.")
+elif answer == "no":
+    print("You answered No.")
+else:
+    print("Invalid input. Please enter Yes or No.")
